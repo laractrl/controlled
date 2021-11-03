6f5eef39c71ba5586eb25f73622e0c2b41e64260
salahhusa9 <salahhusa9@gmail.com>
2021-11-03 17:12:14 +0100

    Change controller class

--- routes/controlled.php
+++ routes/controlled.php
@@ -4,4 +4,4 @@ use Controlled\ControlledController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('test/confirme', [ ControlledController::class ,'confirme' ]);
-Route::get('Locked', [ LinkController::class ,'locked' ])->name('locked');
+Route::get('Locked', [ ControlledController::class ,'locked' ])->name('locked');
