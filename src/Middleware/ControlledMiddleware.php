145a1b8b3fdb37b7bf52d6eb4e66e32e15018d14
salahhusa9 <salahhusa9@gmail.com>
2022-02-15 14:48:45 +0100

    Invalid typehint detected in function

--- src/Middleware/ControlledMiddleware.php
+++ src/Middleware/ControlledMiddleware.php
@@ -20,7 +20,7 @@ class ControlledMiddleware
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next) : mixed
     {
         $url = $request->getPathInfo();
 
