3e21c12cccd3d3860a2f30ba203f0081c5e454ca
salahhusa9 <salahhusa9@gmail.com>
2022-03-15 17:14:57 +0100

    change in config file

--- config/controlled.php
+++ config/controlled.php
@@ -2,7 +2,8 @@
 
 return [
     /*
-    / Url Recheck
+    / It is recommended to set the urls that the user of your application must pass, 
+    / don't set '/' must be use a '/login' url or POST urls.
     */
     'urls' => [
         '/login'
