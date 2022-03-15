5d5711992e8514e6f910d4d73b2136f93f97bf2d
salahhusa9 <salahhusa9@gmail.com>
2022-03-15 15:24:34 +0100

    Undefined function call detected (PHP-E1000), Function request not found : appIP() 500

--- src/helpers/functions.php
+++ src/helpers/functions.php
@@ -6,18 +6,14 @@
  * @return void
  */
 
-if (function_exists('appDomain')) {
-    return;
-} else {
+if (!function_exists('appDomain')){
     function appDomain()
     {
         return request()->getHost();
     }
 }
 
-if (function_exists('appIP')) {
-    return;
-} else {
+if (!function_exists('appIP')){
     function appIP()
     {
         return request()->server('SERVER_ADDR', $_SERVER['SERVER_ADDR'] ?? null);
