627206183e9faea15bfb525f8850093b624f7a33
salahhusa9 <salahhusa9@gmail.com>
2022-03-21 13:37:52 +0100

    change

--- src/ControlledController.php
+++ src/ControlledController.php
@@ -40,7 +40,6 @@ class ControlledController extends Controller
     public function closed()
     {
         // $view = config('controlled.view', null) ?? 'controlled::closed';
-
         // info($view);
         // return view($view);
         return view('controlled::closed');
