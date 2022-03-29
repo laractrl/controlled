19f4c29220621d398396f9d75b7eccf6b3e88278
salahhusa9 <salahhusa9@gmail.com>
2022-03-29 17:24:39 +0000

    Facades Func status : getFacadeAccessor

--- src/Facades/LaraApp.php
+++ src/Facades/LaraApp.php
@@ -2,12 +2,13 @@
 
 namespace Controlled\Facades;
 
+use Controlled\LaraApp as ControlledLaraApp;
 use Illuminate\Support\Facades\Facade;
 
 class LaraApp extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'LaraApp';
+        return ControlledLaraApp::class;
     }
 }
